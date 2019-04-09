3473bece9cd01abb0f607f0844b446a74510f292
Frederik Kær <Shikanoths@gmail.com>
2019-04-09 10:36:12 +0200

    Projekt Skelet
    Dummy project for master

---
+++ HypersWebshop/HypersWebshop.BusinessLogic/Interfaces/ICRUD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HypersWebshop.BusinessLogic
+{
+    interface ICRUD<T>
+    {
+        void create(T entity);
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        void Update(T entity);
+        void Delete(int id);
+    }
+}
