1bd170277ebed506c68056211d69f5ece41c924e
Lasse <lasselyngdala@gmail.com>
2019-04-15 13:34:01 +0200

    DBConnect
    Fungerer kun på Lasse's PC fordi den bruger en lokal CoolShop database

--- HypersWebshop/HypersWebshop.DataAccessLayer/DBConnection.cs
+++ HypersWebshop/HypersWebshop.DataAccessLayer/DBConnection.cs
@@ -7,7 +7,38 @@ using System.Data.SqlClient;
 
 namespace HypersWebshop.DataAccessLayer
 {
-    class DBConnection
+    public class DBConnection
     {
+        private string _connectionString = "Data Source=.\\SQLExpress;Initial Catalog = CoolShop; Integrated Security = True";
+        SqlConnection connection;
+
+        public void OpenConnection()
+        {
+            connection = new SqlConnection(_connectionString);
+            connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            connection.Close();
+        }
+
+        public void ExecuteQueries(string query)
+        {
+            OpenConnection();
+            //string ps = "insert into Product values ('7gag', 2600);";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            // cmd.Prepare();
+            cmd.ExecuteNonQuery();
+        }
+
+        public SqlDataReader DataReader(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, connection);
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+
+
     }
 }
