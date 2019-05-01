f21296e472f235ed4440593afa022f8e68e45830
Frederik Kær <Shikanoths@gmail.com>
2019-05-01 14:51:04 +0200

    Flyttet vores website over i anden mappe.
    Skal integreres med Updateret DB/ DAL før det virker. 
    Der er forbindelse igennem

---
+++ HypersWebApp/HypersWebApp/HypersWebApp/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace HypersWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        ServiceReference1.IProductInterface productInterface = new ServiceReference1.ProductInterfaceClient();
+        public ActionResult Harddisk()
+        {
+            //Under udvikling stadig da database / kodeopbugningen i den anden solution er under opbygning
+            ViewBag.Message = productInterface.FindProduct(1).Name;
+            return View();
+        }
+        public ActionResult Ram()
+        {
+            return View();
+        }
+        public ActionResult Batteri()
+        {
+            return View();
+        }
+        public ActionResult CPU()
+        {
+            return View();
+        }
+        public ActionResult CPU_Køling()
+        {
+            return View();
+        }
+        public ActionResult GPU()
+        {
+            return View();
+        }
+        public ActionResult Motherboard()
+        {
+            return View();
+        }
+        public ActionResult Optisk_Drev()
+        {
+            return View();
+        }
+        public ActionResult Strømforsyning()
+        {
+            return View();
+        }
+    }
+}
